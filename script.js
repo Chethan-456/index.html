594c564317bba08384490c8210aeb8fccc8b4fbd
Chethan-456 <chethanpashupathy@gmail.com>
2024-09-03 13:01:00 +0530

    solved

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleFAQ(element) {
+    const answer = element.querySelector('.faq-answer');
+    const arrow = element.querySelector('.arrow');
+
+    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    arrow.classList.toggle('rotate');
